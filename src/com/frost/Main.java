d8f4698b57bf1f5953b1bc96752c4f73ee258700
frost8111981 <73012443+frost8111981@users.noreply.github.com>
2021-03-02 20:59:16 +0300

    Initial commit

---
+++ src/com/frost/Main.java
@@ -0,0 +1,40 @@
+package com.frost;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String[] cats = new String[]{"васька", "мурка", "дымка", "рыжик", "серый", "снежок", "босс", "борис", "визя", "гарфи"};
+
+        Map<String, Cat> map = addCatsToMap(cats);     // коллекция на вывод через метод
+
+        for (Map.Entry<String, Cat> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " - " + pair.getValue());
+        }
+    }
+
+
+    public static Map<String, Cat> addCatsToMap(String[] cats) {
+        Map<String, Cat> result = new HashMap<>();
+        for(int i =0; i< cats.length; i++){
+            result.put(cats[i], new Cat(cats[i]));
+        }
+        return result;
+    }
+
+
+    public static class Cat {
+        String name;
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name != null ? name.toUpperCase() : null;
+        }
+    }
+}
